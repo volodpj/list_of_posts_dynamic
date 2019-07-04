3b6508cec0936c046269fbb3ad4632ef52e8fa66
volodpj <volod.af@gmail.com>
2019-07-04 17:36:27 +0300

    work over filter v1

--- src/components/postList/PostsList.js
+++ src/components/postList/PostsList.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import Post from '../post/Post'
 
-const PostsList = ({posts}) => {
-    return (
-        <div>
-            <input itemType='text' style={{width: '500px', borderRadius: "7px"}}></input> 
-            <ul>
-                {posts.map((post) => {
-                    return <Post key={post.id} post={post}/>
-                })}
-            </ul>
-        </div>
-    )
+class PostsList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            findText: '',
+            filter
+        };
+
+      
+    }
+    giveFindText = (event) => {
+        const text = event.target.value;
+        this.setState({
+            findText: text
+        })
+    }
+    
+
+    render() {
+        console.log(this.state.findText)
+        return (
+            <div>
+                <input type='text' style={{width: '500px', borderRadius: "7px"}} 
+                    onChange={this.giveFindText}
+                ></input> 
+                <ul>
+                    {this.props.posts.map((post) => {
+                        return <Post key={post.id} post={post}/>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+    
 };
 
 export default PostsList
